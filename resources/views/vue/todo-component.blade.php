00d6543734501edaac0f1489f022fe8f226ee1be
believe <rubyrails.life@gmail.com>
2019-04-28 22:28:54 +0900

    Vue.js(route: /todo-component)

--- resources/views/vue/todo-component.blade.php
+++ resources/views/vue/todo-component.blade.php
@@ -1,17 +1,6 @@
-<!doctype html>
-  <html lang="{{ app()->getLocale() }}">
-    <head>
-      <meta charset="utf-8">
-      <meta name="csrf-token" content="{{ csrf_token() }}">
-      <meta http-equiv="X-UA-Compatible" content="IE=edge">
-      <meta name="viewport" content="width=device-width, initial-scale=1">
-      <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-      <title>Laravel-Vue-todo</title>
-    </head>
-    <body>
-      <div id="app">
-        <todo-component></todo-component>
-      </div>
-      <script src="{{ asset('js/app.js') }}"></script>
-    </body>
-</html>
+@extends('layouts.proj')
+@section('content')
+  <div id="app">
+    <todo-component></todo-component>
+  </div>
+@stop      
